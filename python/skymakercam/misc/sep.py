4e1e61ed9d28ecefa3408854af6dc8b67d5a4a03
Florian Briegel <wasndas@zone42.de>
2021-08-16 19:15:51 +0200

    improved matplotlib com

--- python/skymakercam/misc/sep.py
+++ python/skymakercam/misc/sep.py
@@ -2,62 +2,67 @@ import numpy as np
 import sep
 
 from astropy.io import fits
+
+
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from matplotlib.patches import Ellipse
-
-
+from  matplotlib import use as plt_use
+plt_use('TkAgg')
+plt_use('Qt5Agg')
+plt.ion()
+plt.show()
+    
 rcParams['figure.figsize'] = [10., 8.]
+cmap='gist_heat'
 
 hdu = fits.open("../20191010/sx.hws.pcam.20191010_00001.fits")
 
 data = hdu[0].data[0]
 
 m, s = np.mean(data), np.std(data)
-plt.imshow(data, interpolation='nearest', cmap='gray', vmin=m-s, vmax=m+s, origin='lower')
-plt.colorbar()
-plt.title('matplotlib.pyplot.imshow() function Example', 
-                                     fontweight ="bold")
-plt.show(block=False)
+plot_data = plt.imshow(data, interpolation='nearest', cmap=cmap, vmin=m-s, vmax=m+s, origin='lower')
+plot_cb = plt.colorbar(cmap=cmap, orientation='vertical')
+plt.title('skymaker cam', fontweight ="bold")
 
+plt.draw()
 
 bkg = sep.Background(data.byteswap().newbyteorder())
 
 data_sub = data - bkg
 
 m, s = np.mean(data_sub), np.std(data_sub)
-plt.imshow(data_sub, interpolation='nearest', cmap='gray', vmin=m-s, vmax=m+s, origin='lower')
-plt.show(block=False)
+lp, up = np.percentile(data_sub,0.5), np.percentile(data_sub,99.8)
 
+plot_data.set_data(data_sub)
+plot_data.set_clim(lp, up)
 
-lp, up = np.percentile(data_sub,5.0), np.percentile(data_sub,99.995)
-plt.imshow(data_sub, interpolation='nearest', cmap='gray', vmin=lp, vmax=up, origin='lower')
-plt.show(block=False)
-
-
-# plot background-subtracted image
-#fig, ax = plt.subplots()
+#ax = plt.gcf().gca()
+ax = plot_data.get_figure().gca()
 
 objects = sep.extract(data_sub, 1.5, err=bkg.globalrms)
 
-ax = plt.gcf().gca()
 # plot an ellipse for each object
+
 for i in range(len(objects)):
     e = Ellipse(xy=(objects['x'][i], objects['y'][i]),
                 width=6*objects['a'][i],
                 height=6*objects['b'][i],
                 angle=objects['theta'][i] * 180. / np.pi)
     e.set_facecolor('none')
-    e.set_edgecolor('red')
+    e.set_edgecolor('yellow')
     ax.add_artist(e)
 
-plt.show(block=False)  
-
-
+while len(ax.artists):
+   for a in ax.artists:
+       a.remove()
+    
 from scipy.ndimage.filters import gaussian_filter
 
-data_blur = gaussian_filter(data_sub, sigma=7)
+data_blur = gaussian_filter(data_sub, sigma=70)
 
 m, s = np.mean(data_blur), np.std(data_blur)
-plt.imshow(data_blur, interpolation='nearest', cmap='gray', vmin=m-s, vmax=m+s, origin='lower')
-plt.show(block=False)
+plot_data.set_clim(m-s, m+s)
+plot_data.set_data(data_blur)
+
+
