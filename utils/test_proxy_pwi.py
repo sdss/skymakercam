3847d6377d9a5b818df662581c36423465d49614
Florian Briegel <wasndas@zone42.de>
2021-12-06 11:55:46 +0100

    0.0.1

--- utils/test_proxy_pwi.py
+++ utils/test_proxy_pwi.py
@@ -2,49 +2,40 @@
 #
 # @Author: Florian Briegel (briegel@mpia.de)
 # @Date: 2021-08-18
-# @Filename: proxy_test.py
+# @Filename: test_proxy_pwi.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
-import uuid
+import asyncio
 import sys
+import uuid
+
+from logging import DEBUG
 
 from clu import AMQPClient, CommandStatus
-from cluplus.proxy import Proxy, ProxyException, ProxyPlainMessagException, invoke, unpack
+from test_lvm_actors import LVM
 
-import asyncio
+from cluplus.proxy import Proxy, invoke, unpack
 
 
-async def test_simple_pwi_ctrl():
-    
-    consumer = "lvm.sci.pwi"
-    amqpc = AMQPClient(name=f"{sys.argv[0]}.proxy-{uuid.uuid4().hex[:8]}")
-    await amqpc.start()
-    
-    lvm_sci_pwi=Proxy(consumer, amqpc)
+amqpc = AMQPClient(name=f"{sys.argv[0]}.proxy-{uuid.uuid4().hex[:8]}")
 
-    try:
-      
-        ret = await unpack(lvm_sci_pwi.help())
-       
-        await lvm_sci_pwi.setConnected(True)
-        
-        await lvm_sci_pwi.setEnabled(True)
-        
-        isTracking = await unpack(lvm_sci_pwi.setTracking(True))
-        assert isTracking == True
-        
-        # lets define a callback for status updates.
-        def callback(reply): amqpc.log.warning(f"Reply: {CommandStatus.code_to_status(reply.message_code)} {reply.body}")
+lvm_sci_pwi = Proxy(amqpc, LVM.SCI.PWI).start()
 
-        # await lvm_sci_pwi.park(callback=callback)
-        
-        await lvm_sci_pwi.gotoRaDecJ2000(10, 20, callback=callback)
-        
-        # we do use send_command/click options without --, it will be added internally
-        await lvm_sci_pwi.offset(ra_add_arcsec=10)
-        
-    except Exception as e:
-        amqpc.log.warning(f"Exception: {e}")
+lvm_sci_pwi.setConnected(True)
 
-asyncio.run(test_simple_pwi_ctrl())
+isEnabled = unpack(lvm_sci_pwi.setEnabled(True), "is_enabled")
+assert isEnabled == True
+        
+isTracking = unpack(lvm_sci_pwi.setTracking(True))
+assert isTracking == True
+
+amqpc.log.sh.setLevel(DEBUG)        
+        
+def callback(reply): 
+    amqpc.log.warning(f"{CommandStatus.code_to_status(reply.message_code)} {reply.body}")
+
+lvm_sci_pwi.gotoRaDecJ2000(10, 20, callback=callback)
+
+lvm_sci_pwi.offset(ra_add_arcsec = 10)
+        
 
--- utils/test_proxy_tan.py
+++ utils/test_proxy_tan.py
@@ -5,173 +5,43 @@
 # @Filename: proxy_test.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
-import uuid
 import sys
+import uuid
+from logging import DEBUG
 
 from clu import AMQPClient, CommandStatus
-from cluplus.proxy import Proxy, ProxyException, ProxyPlainMessagException, invoke, unpack
+from test_lvm_actors import LVM
 
-import asyncio
-
-
-async def test_single_param_return():
-    
-    consumer = "lvm.sci.foc"
-    amqpc = AMQPClient(name=f"{sys.argv[0]}.proxy-{uuid.uuid4().hex[:8]}")
-    await amqpc.start()
-    
-    lvm_sci_foc=Proxy(consumer, amqpc)
-    amqpc.log.warning(sys.argv[0])
-        
-    amqpc.log.warning(f'#1: {await unpack(lvm_sci_foc.isReachable())}')
-    pos, unit = await unpack(lvm_sci_foc.getDeviceEncoderPosition("UM"))
-    amqpc.log.warning(f'#2: {pos}, {unit}')
-
-    amqpc.log.warning(f'#3: {(await invoke(lvm_sci_foc.getPosition())).Position}')
-
-    pos = await invoke(lvm_sci_foc.getDeviceEncoderPosition("UM"))
-    amqpc.log.warning(f'#4: {pos.DeviceEncoderPosition}')
-
-    pos = await unpack(lvm_sci_foc.getDeviceEncoderPosition("UM"))
-    amqpc.log.warning(f'#5 {pos}')
-
-    amqpc.log.warning(f'#6: {await invoke(lvm_sci_foc.getNamedPosition(1))}')
-
-    try:
-        await invoke(lvm_sci_foc.getNamedPosition(1), lvm_sci_foc.getNamedPosition(10), lvm_sci_foc.getNamedPosition(12))
-    except ProxyException as e:
-        amqpc.log.warning(f"Exception: {e}")
-
-    try:
-        await invoke(lvm_sci_foc.getNamedPosition(10))
-    except Exception as e:
-        amqpc.log.warning(f"Exception: {e}")
-
-    try:
-        await unpack(lvm_sci_foc.getDeviceEncoderPositin("UM"))
-    except Exception as e:
-        amqpc.log.warning(f"Exception: {e}")
+from cluplus.proxy import Proxy, invoke, unpack
 
 
-asyncio.run(test_single_param_return())
+amqpc = AMQPClient(name=f"{sys.argv[0]}.client-{uuid.uuid4().hex[:8]}")
+
+lvm_sci_foc = Proxy(amqpc, LVM.SCI.FOC).start()
+lvm_skyw_foc = Proxy(amqpc, LVM.SKYW.FOC).start()
+lvm_skye_foc = Proxy(amqpc, LVM.SKYE.FOC).start()
+lvm_spec_foc = Proxy(amqpc, LVM.SPEC.FOC).start()
+
+invoke(
+    lvm_sci_foc.async_moveToLimit(-1),
+    lvm_skye_foc.async_moveToLimit(1),
+    lvm_skyw_foc.async_moveToLimit(-1),
+    lvm_spec_foc.async_moveToLimit(1),
+)
+
+amqpc.log.sh.setLevel(DEBUG)
 
 
-async def test_single_param_return():
-    
-    consumer = "lvm.sci.foc"
-    amqpc = AMQPClient(name=f"{sys.argv[0]}.client-{uuid.uuid4().hex[:8]}")
-    await amqpc.start()
-    
-    lvm_sci_foc=Proxy(consumer, amqpc)
-    
-    amqpc.log.warning(f'#1: {(await invoke(lvm_sci_foc.isReachable())).Reachable}')
-    
-    amqpc.log.warning(f'#2: {await unpack(lvm_sci_foc.isReachable())}')
-    amqpc.log.warning(f'#3: {await unpack(lvm_sci_foc.getDeviceEncoderPosition("UM"))}')
-    
-asyncio.run(test_single_param_return())
+def callback(reply):
+    amqpc.log.debug(f"{CommandStatus.code_to_status(reply.message_code)}"
+                    f"{reply.sender}:  {reply.body}")
 
 
-async def test_callback_and_blocking():
-    
-    consumer = "lvm.sci.foc"
-    amqpc = AMQPClient(name=f"{sys.argv[0]}.client-{uuid.uuid4().hex[:8]}")
-    await amqpc.start()
-    
-    lvm_sci_foc=Proxy(consumer, amqpc)
-    
-    def toHome_callback(reply): amqpc.log.warning(f"Reply: {CommandStatus.code_to_status(reply.message_code)} {reply.body}")
-    await lvm_sci_foc.moveToHome(callback=toHome_callback)
-    
-    def toLimit_callback(reply): amqpc.log.warning(f"Reply: {CommandStatus.code_to_status(reply.message_code)} {reply.body}")
-    await lvm_sci_foc.moveToLimit(-1, callback=toLimit_callback)
+rc = invoke(
+    lvm_sci_foc.async_moveToHome(callback=callback),
+    lvm_skye_foc.async_moveToHome(callback=callback),
+    lvm_skyw_foc.async_moveToHome(callback=callback),
+    lvm_spec_foc.async_moveToHome(callback=callback),
+)
 
-
-
-asyncio.run(test_callback_and_blocking())
-
-async def test_tasks():
-    
-    amqpc = AMQPClient(name=f"{sys.argv[0]}.client-{uuid.uuid4().hex[:8]}")
-    await amqpc.start()
-    
-    lvm_sci_foc=Proxy("lvm.sci.foc", amqpc)
-    lvm_skye_foc=Proxy("lvm.skye.foc", amqpc)
-    lvm_skyw_foc=Proxy("lvm.skyw.foc", amqpc)
-    lvm_spec_foc=Proxy("lvm.spec.foc", amqpc)
-    
-    
-    ret = await invoke(
-            lvm_sci_foc.moveToHome(),
-            lvm_skye_foc.moveToHome(),
-            lvm_skyw_foc.moveToHome(),
-            lvm_spec_foc.moveToHome(),
-          )
-    amqpc.log.warning(f"{ret}")
-    
-    ret = await invoke(
-                lvm_sci_foc.moveToLimit(-1),
-                lvm_skye_foc.moveToLimit(-1),
-                lvm_skyw_foc.moveToLimit(-1),
-                lvm_spec_foc.moveToLimit(-1),
-            )
-    amqpc.log.warning(f"{ret}")
-
-asyncio.run(test_tasks())
-
-async def test_callback_and_parallel_classic():
-    
-    amqpc = AMQPClient(name=f"{sys.argv[0]}.client-{uuid.uuid4().hex[:8]}")
-    await amqpc.start()
-    
-    lvm_sci_foc=Proxy("lvm.sci.foc", amqpc)
-    lvm_skye_foc=Proxy("lvm.skye.foc", amqpc)
-    
-    def toHome_callback(reply): amqpc.log.warning(f"Reply: {CommandStatus.code_to_status(reply.message_code)} {reply.body}")
-    lvm_sci_foc_future = await lvm_sci_foc.moveToHome(callback=toHome_callback, blocking=False)
-    lvm_skye_foc_future = await lvm_skye_foc.moveToHome(callback=toHome_callback, blocking=False)
-    await lvm_sci_foc_future
-    await lvm_skye_foc_future
-    
-    def toLimit_callback(reply): amqpc.log.warning(f"Reply: {CommandStatus.code_to_status(reply.message_code)} {reply.body}")
-    lvm_sci_foc_future = await lvm_sci_foc.moveToLimit(-1, callback=toLimit_callback, blocking=False)
-    lvm_skye_foc_future = await lvm_skye_foc.moveToLimit(-1, callback=toLimit_callback, blocking=False)
-    await lvm_sci_foc_future
-    await lvm_skye_foc_future
-    
-
-asyncio.run(test_callback_and_parallel_classic())
-
-async def test_callback_and_gather_raw():
-    
-    amqpc = AMQPClient(name=f"{sys.argv[0]}.client-{uuid.uuid4().hex[:8]}")
-    await amqpc.start()
-    
-    lvm_sci_foc=Proxy("lvm.sci.foc", amqpc)
-    lvm_skye_foc=Proxy("lvm.skye.foc", amqpc)
-    lvm_skyw_foc=Proxy("lvm.skyw.foc", amqpc)
-    lvm_spec_foc=Proxy("lvm.spec.foc", amqpc)
-    
-    
-    ret = await asyncio.gather(
-            await lvm_sci_foc.moveToHome(blocking=False),
-            await lvm_skye_foc.moveToHome(blocking=False),
-            await lvm_skyw_foc.moveToHome(blocking=False),
-            await lvm_spec_foc.moveToHome(blocking=False),
-          )
-    amqpc.log.warning(f"{ret}")
-    
-    ret = await asyncio.gather(
-                    await lvm_sci_foc.moveToLimit(-1, blocking=False),
-                    await lvm_skye_foc.moveToLimit(-1, blocking=False),
-                    await lvm_skyw_foc.moveToLimit(-1, blocking=False),
-                    await lvm_spec_foc.moveToLimit(-1, blocking=False),
-                  )
-    amqpc.log.warning(f"{ret}")
-
-asyncio.run(test_callback_and_gather_raw())
-
-
-
-
-
+amqpc.log.info(f"{unpack(rc, 'AtHome')}")
